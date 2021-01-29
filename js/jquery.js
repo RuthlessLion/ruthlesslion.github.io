c739fae8f6cd8da8b1bee6c3ee8d3073e1efef88
Leo <windowtheworldofgames@gmail.com>
2021-01-29 12:50:34 +0700

    Update

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,8 @@
 $(function(){
     $(".hello").fadeTo(1500, 1);
+    $(".hello2").fadeTo(2000, 1);
     $(".hello").fadeOut(1500, 0);
+    $(".hello2").fadeOut(1000, 0);
 
     setTimeout(function(){
         $(".contact").fadeTo(1500, 1);
